eb1e54617786d254537e70bc9f92e1d2e8e29ad8
lyscf <69514837+lyscf@users.noreply.github.com>
2024-02-16 21:13:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="r2.amarket.dev";
+        url.hostname="r2.lzu.icu";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
